0e19dd0599ee505a7c02a43725fc10b04f7f5a1a
Diane Galiano <dianegaliano09@gmail.com>
2021-09-04 23:21:17 -0500

    fixed api key issue

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//API Key
 var apiKey = "7477daa72c434d694f88ffb36209cee3";
 
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey, {
-  method: "GET",
+//Fetch API
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat=97.7278&lon=31.1171&appid=" + apiKey, {
+method: "GET"
 })
   .then((response) => { 
     console.log(response); 
