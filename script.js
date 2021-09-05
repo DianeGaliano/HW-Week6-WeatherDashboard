8da268e51036e65c2075d81f9bc822b68eb038fa
Diane Galiano <dianegaliano09@gmail.com>
2021-09-04 22:38:31 -0500

    added more to the api fetch function

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=";
-  
-  fetch(requestUrl)
-  .then(function (response) {
+var apiKey = "7477daa72c434d694f88ffb36209cee3";
+
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey, {
+  method: "GET",
+})
+  .then((response) => { 
+    console.log(response); 
     return response.json();
   })
   .then(function (data) {
     console.log(data)
+  })
+  .catch((err) => {
+    console.error(err);
   });
