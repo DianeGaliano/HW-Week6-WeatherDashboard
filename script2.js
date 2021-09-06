b8e12744b606a6f5be738598a0caa7e5068f0293
Diane Galiano <dianegaliano09@gmail.com>
2021-09-06 17:46:46 -0500

    restarted on script file added a fetch and get funcion

--- script2.js
+++ script2.js
@@ -1,21 +1,77 @@
 var apiKey = "7477daa72c434d694f88ffb36209cee3";
-var city;
-var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-fetch (url), {
-   method: "GET",
-}
-.then((response) => {
-    console.log(response);
-    return response.jason();
-})
-.then(function(data) {
-    console.log(data);  
-    
-})
-.catch((err) => {
-    console.error(err);
-});
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city");
+var repoContainerEl = document.querySelector("repos-container");
+
 
 //Search Button
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
 
+    if (city) {
+        gitCityRepos(city);
+
+        repoContainerEl.textContent = "";
+        cityInputEl.value = "";
+    }else {
+        alert("Please enter city")
+    }
+};
+
+//Fetch Function
+
+var getWeatherRepos = function (weather) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+  
+    fetch(url)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data);
+            displayRepos(data, weather);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert("Unable to get weather");
+      });
+  };
+
+var displayRepos = function (repos) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No Weather Found";
+        return;
+    }
+    for (var i = 0; i < repos.length; i++) {
+        // The result will be `<github-username>/<github-repository-name>`
+        var repoName = repos[i].owner.login + '/' + repos[i].name;
+    
+        var repoEl = document.createElement('div');
+        repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    
+        var titleEl = document.createElement('span');
+        titleEl.textContent = repoName;
+    
+        repoEl.appendChild(titleEl);
+    
+        var statusEl = document.createElement('span');
+        statusEl.classList = 'flex-row align-center';
+    
+        if (repos[i].open_issues_count > 0) {
+          statusEl.innerHTML =
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+        } else {
+          statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+    
+        repoEl.appendChild(statusEl);
+    
+        repoContainerEl.appendChild(repoEl);
+      }
+
+}
