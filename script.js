f5fafec3d314bc296edf046e3b935c80231df0ec
Diane Galiano <dianegaliano09@gmail.com>
2021-09-06 20:32:39 -0500

    created a search function

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//Form
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#weather-container");
+var citySearchTerm = document.querySelector("#city-search");
+
+
+
+
+//API
+var cityNfameEl = document.querySelector("#city-name");
+var weatherContainerEl = document.querySelector("#weather-container");
+
+var userFormEl = document.querySelector()
 var apiKey = "7477daa72c434d694f88ffb36209cee3";
 
 fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04" + "&appid=" + apiKey, {
@@ -38,3 +51,28 @@ var getCityWeather = function (onecall) {
   });
 };
 
+var displayWeather = function (onecall) {
+  if (onecall.length === 0) {
+    weatherContainerEl.textContent = "Weather for:"
+    return;
+  }
+
+  for (var i = 0; i < onecall.length; i++) {
+    var onecallEl = document.createElement("a");
+    onecallEl.classList = 'list-item flex-row justify-space-between align-center'
+
+    onecallEl.setAttribute("herf", onecall[i].html_url);
+    onecallEl.setAttribute("target", "_blank");
+
+    var dailyEl = document.createElement("span");
+    dailyEl.textContent = onecall[i].daily;
+    onecallEl.appendChild(dailyEl);
+
+    var currentEl = document.createElement("span");
+    onecallEl.appendChild(currentEl);
+    weatherContainerEl.appendChild(onecallEl);
+
+  }
+};
+
+getCityName()
